2d93da6be2437552096ba171dcb44f436877fdbc
shafie-isak <shafiewisak@gmail.com>
2024-11-26 17:54:30 +0300

    add header and hero section

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+
+<header>
+    <div class="container header">
+        <a href="#">
+            <img src="assets/image/logo.png" alt="logo">
+
+        </a>
+        <nav>
+            <ul>
+                <li><a href="#" class="active">Home</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Tickets</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+        <div class="registrLogin">
+            <a href="#">Register</a>
+            <button>Login</button>
+        </div>
+    </div>
+</header>
