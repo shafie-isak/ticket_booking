616d141bcb14d424c08fc8138e9e0cf074b59c78
shaafici51 <shafiabdi104@gmail.com>
2024-11-27 13:36:56 +0300

    some change

---
+++ includes/footer.php
@@ -0,0 +1,28 @@
+<footer>
+    <div class="container">
+        <div class="footer">
+            <img src="assets/images/logo_white.png" width="250" alt="logo">
+            <nav>
+                <p>Quick access</p>
+                <ul>
+                    <li><a href="#" class="active">Home</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Tickets</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+            <div class="message">
+                <p>Quick message</p>
+                <form>
+                    <input type="text" placeholder="Email...">
+                    <textarea name="message" id="message" placeholder="messge..."></textarea>
+                    <button>Send</button>
+                </form>
+            </div>
+        </div>
+        <hr>
+        <p class="copyright">&copy 2024 | All copyright reserved by tiketBook</p>
+    </div>
+    
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,21 @@
+
+<header>
+    <div class="container header">
+        <a href="#">
+            <img src="assets/images/logo_white.png" alt="logo">
+
+        </a>
+        <nav>
+            <ul>
+                <li><a href="#" class="active">Home</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Tickets</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+        <div class="registrLogin">
+            <button>Login/Join</button>
+        </div>
+    </div>
+</header>
