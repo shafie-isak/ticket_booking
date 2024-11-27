3c69d86a98fb7faaaab989a07044292b295fb60d
shaafici51 <shafiabdi104@gmail.com>
2024-11-27 16:30:55 +0300

    views added

--- index.php
+++ index.php
@@ -11,39 +11,8 @@
 <body>
 
     <?php 
-        include ("routes/about.php");
+        include ("views/home.php");
     ?>
 <script src="assets/js/main.js"></script>
 </body>
 </html>
-
-
-
-<!-- <?php -->
-
-// include 'config/database.php';
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
-
-// include 'controlers/user.php';
-
-// $action = $_GET['action'] ?? 'showRegisterForm';  
-
-// switch ($action) {
-//     case 'register_user':
-//         $controller = new UserController($conn); 
-//         $controller->registerUser(); 
-//         break;
-
-//     case 'showRegisterForm':
-//         $controller = new UserController($conn); // Create a new UserController instance
-//         $controller->showRegisterForm(); // Call the showRegisterForm method to display the registration form
-//         break;
-
-//     default:
-//         echo "Welcome to the MVC Ticket System!"; // Default action if no specific action is given
-//         break;
-// }
-
-// ?>
