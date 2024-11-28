7b4065c1bd870d15b45289c0367793de6ea1f535
Bile6 <bileabdulkadirmohamed@gmail.com>
2024-11-28 13:10:39 +0300

    add booking form page

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>Ticket Booking</title>
 
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/ticketbooking.css">
     <link rel="stylesheet" href="assets/css/login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
@@ -26,6 +27,9 @@
                 break;
             case '/contact':
                 include ("views/contact.php");
+                break;
+            case '/ticketbooking':
+                include ("views/ticketbooking.php");
                 break;  
 
             case '/login':
--- views/tickets.php
+++ views/tickets.php
@@ -1,72 +1,88 @@
 <?php
   include('includes/header.php');  
-?>
 
+  if (isset($_POST['book_now'])) {
+    header('Location: /ticketbooking');
+  }
+
+        
+?>
 <section class="ticketsSection">
-  <h1 class="sectionHeader">Available Tickets</h1>
-  <div class="container tickets">
-    <div class="ticketCard">
-      <div class="imgHolder">
-        <img src="assets/images/Jazeera_beach_view.jpg" alt="liido">
+  <h1 class="sectionHeader">Popular Tickets</h1>
+    <div class="container tickets">
+      <div class="ticketCard">
+        <div class="imgHolder">
+          <img src="assets/images/Jazeera_beach_view.jpg" alt="liido">
+        </div>
+        <div class="ticketcaption">
+          <h1>Jaziiro beach</h1>
+          <p>Jaziiro Beach is a hidden gem known for its serene atmosphere and stunning natural beauty. With its soft sands and clear waters, it's a peaceful retreat for those looking to escape the crowds and enjoy the tranquil surroundings.</p>
+          <form method="POST">
+                <button type="submit" name="book_now">Book now</button>
+            </form>
+        </div>
       </div>
-      <div class="ticketcaption">
-        <h1>Jaziiro beach</h1>
-        <p>Jaziiro Beach is a hidden gem known for its serene atmosphere and stunning natural beauty. With its soft sands and clear waters, it's a peaceful retreat for those looking to escape the crowds and enjoy the tranquil surroundings.</p>
-        <button>Book now</button>
+      <div class="ticketCard">
+        <div class="imgHolder">
+          <img src="assets/images/liido.jpg" alt="liido">
+        </div>
+        <div class="ticketcaption">
+          <h1>Liido beach</h1>
+          <p>Liido Beach offers pristine sands and crystal-clear waters, making it a perfect spot for relaxation and seaside enjoyment.</p>
+          <form method="POST">
+                <button type="submit" name="book_now">Book now</button>
+            </form>
+        </div>
+      </div>
+      <div class="ticketCard">
+        <div class="imgHolder">
+          <img src="assets/images/Jazeera_beach_view.jpg" alt="liido">
+        </div>
+        <div class="ticketcaption">
+          <h1>Jaziiro beach</h1>
+          <p>Jaziiro Beach is a hidden gem known for its serene atmosphere and stunning natural beauty. With its soft sands and clear waters, it's a peaceful retreat for those looking to escape the crowds and enjoy the tranquil surroundings.</p>
+          <form method="POST">
+                <button type="submit" name="book_now">Book now</button>
+            </form>
+        </div>
+      </div>
+      <div class="ticketCard">
+        <div class="imgHolder">
+          <img src="assets/images/liido.jpg" alt="liido">
+        </div>
+        <div class="ticketcaption">
+          <h1>Liido beach</h1>
+          <p>Liido Beach offers pristine sands and crystal-clear waters, making it a perfect spot for relaxation and seaside enjoyment.</p>
+          <form method="POST">
+                <button type="submit" name="book_now">Book now</button>
+            </form>
+        </div>
+      </div>
+      <div class="ticketCard">
+        <div class="imgHolder">
+          <img src="assets/images/Jazeera_beach_view.jpg" alt="liido">
+        </div>
+        <div class="ticketcaption">
+          <h1>Jaziiro beach</h1>
+          <p>Jaziiro Beach is a hidden gem known for its serene atmosphere and stunning natural beauty. With its soft sands and clear waters, it's a peaceful retreat for those looking to escape the crowds and enjoy the tranquil surroundings.</p>
+          <form method="POST">
+                <button type="submit" name="book_now">Book now</button>
+            </form>
+        </div>
+      </div>
+      <div class="ticketCard">
+        <div class="imgHolder">
+          <img src="assets/images/liido.jpg" alt="liido">
+        </div>
+        <div class="ticketcaption">
+          <h1>Liido beach</h1>
+          <p>Liido Beach offers pristine sands and crystal-clear waters, making it a perfect spot for relaxation and seaside enjoyment.</p>
+          <form method="POST">
+                <button type="submit" name="book_now">Book now</button>
+            </form>
+        </div>
       </div>
     </div>
-    <div class="ticketCard">
-      <div class="imgHolder">
-        <img src="assets/images/liido.jpg" alt="liido">
-      </div>
-      <div class="ticketcaption">
-        <h1>Liido beach</h1>
-        <p>Liido Beach offers pristine sands and crystal-clear waters, making it a perfect spot for relaxation and seaside enjoyment.</p>
-        <button>Book now</button>
-      </div>
-    </div>
-    <div class="ticketCard">
-      <div class="imgHolder">
-        <img src="assets/images/Jazeera_beach_view.jpg" alt="liido">
-      </div>
-      <div class="ticketcaption">
-        <h1>Jaziiro beach</h1>
-        <p>Jaziiro Beach is a hidden gem known for its serene atmosphere and stunning natural beauty. With its soft sands and clear waters, it's a peaceful retreat for those looking to escape the crowds and enjoy the tranquil surroundings.</p>
-        <button>Book now</button>
-      </div>
-    </div>
-    <div class="ticketCard">
-      <div class="imgHolder">
-        <img src="assets/images/liido.jpg" alt="liido">
-      </div>
-      <div class="ticketcaption">
-        <h1>Liido beach</h1>
-        <p>Liido Beach offers pristine sands and crystal-clear waters, making it a perfect spot for relaxation and seaside enjoyment.</p>
-        <button>Book now</button>
-      </div>
-    </div>
-    <div class="ticketCard">
-      <div class="imgHolder">
-        <img src="assets/images/Jazeera_beach_view.jpg" alt="liido">
-      </div>
-      <div class="ticketcaption">
-        <h1>Jaziiro beach</h1>
-        <p>Jaziiro Beach is a hidden gem known for its serene atmosphere and stunning natural beauty. With its soft sands and clear waters, it's a peaceful retreat for those looking to escape the crowds and enjoy the tranquil surroundings.</p>
-        <button>Book now</button>
-      </div>
-    </div>
-    <div class="ticketCard">
-      <div class="imgHolder">
-        <img src="assets/images/liido.jpg" alt="liido">
-      </div>
-      <div class="ticketcaption">
-        <h1>Liido beach</h1>
-        <p>Liido Beach offers pristine sands and crystal-clear waters, making it a perfect spot for relaxation and seaside enjoyment.</p>
-        <button>Book now</button>
-      </div>
-    </div>
-    
-  </div>
 </section>
 
 
