9bb641735cc4f0beab994ad5c49d8faf86ecb91b
shafie-isak <shafiewisak@gmail.com>
2024-11-28 13:19:09 +0300

    marge style.css

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/login.css">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
--- views/userRegistration.php
+++ views/userRegistration.php
@@ -4,20 +4,15 @@
 
 <div class="overlay">
     <div class="login">
-      <div class="imageCover">
-        <img src="assets/images/Register.png" alt="">
-      </div>
-      <div class="loginCover">
-        <h1>Jion Us</h1>
-        <form action="" method="POST">
-            <input type="text" placeholder="name">
-              <input type="email" placeholder="email">
-              <input type="phone" placeholder="phone">
-              <input type="password" placeholder="create password">
-              <button type="submit">Register</button>
-          </form>
-          <p>I have an acount, <a href="#">Login</a></p>
-      </div>
+      <h1>Jion Us</h1>
+      <form action="" method="POST">
+        <input type="text" placeholder="name">
+        <input type="email" placeholder="email">
+        <input type="phone" placeholder="phone">
+        <input type="password" placeholder="create password">
+        <button type="submit">Register</button>
+      </form>
+    <p>I have an acount, <a href="#">Login</a></p>
     </div>
 </div>
 
